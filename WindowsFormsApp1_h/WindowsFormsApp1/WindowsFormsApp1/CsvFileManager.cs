33eaafa717f5dcdefa1fa51db8bf456bd48f0d81
githugacky <acky.c1@gmail.com>
2020-05-01 16:29:10 +0900

    first commit

---
+++ WindowsFormsApp1_h/WindowsFormsApp1/WindowsFormsApp1/CsvFileManager.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Data;
+using System.IO;
+using System.Collections.Generic;
+
+// csvファイル処理に関するメソッド
+namespace WindowsFormsAppParts
+{
+    public partial class CsvFileManager
+    {
+        /// <summary>
+        /// DataTableのデータをcsvファイルに書き込む。
+        /// </summary>
+        /// <param name="dtRows">テーブルの行データ</param>
+        /// <param name="colCount">列数</param>
+        /// <param name="sw">データ保存ファイル</param>
+        public static void CsvFileWrite(DataRowCollection dtRows, int colCount, StreamWriter sw)
+        {
+            
+            foreach (DataRow row in dtRows)
+            {
+                for (int i = 0; i < colCount; i++)
+                {
+                    string field = row[i].ToString();
+                    sw.Write(field);
+                    if (colCount - 1 > i)
+                    // 最終列でない時は","を追加
+                    {
+                        sw.Write(",");
+                    }
+                }
+                // 行末に改行を挿入
+                sw.Write("\r\n");
+            }
+            sw.Close();
+        }
+
+        /// <summary>
+        /// 保存したcsvファイルを読み込む。
+        /// </summary>
+        /// <param name="dataFile">対象csvファイル</param>
+        public static DataTable CsvFileInitialRead(string dataFile)
+        {
+            // 初期設定
+            string csvPath = Directory.GetCurrentDirectory() + "\\" + dataFile;
+
+            DataTable tdTable = new DataTable();
+            
+            tdTable.Columns.Add(new DataColumn("完了", typeof(bool)));
+            tdTable.Columns.Add("期限");
+            tdTable.Columns.Add("やること");
+            tdTable.Columns.Add(new DataColumn("表示", typeof(bool))); ;  // 表示設定（内部データ。画面上非表示にする）
+
+
+            if (File.Exists(csvPath))
+            // 保存済のcsvファイルが存在する場合、ファイルを開く
+            {
+                try
+                {
+                    StreamReader sr = new StreamReader(dataFile);
+
+                    while (!sr.EndOfStream)
+                    {
+                        string line = sr.ReadLine();
+                        string[] items = line.Split(',');
+                        DataRow row = tdTable.NewRow();
+                        tdTable.Rows.Add(items);
+                    }
+
+                    sr.Close();
+                }
+                catch (System.IO.IOException)
+                {
+                    MessageBox.Show("タスク保存ファイルが既に開いています。\r\n" +
+                        "ファイルを閉じてください。", "警告",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.Warning);
+                }
+                
+            }
+
+            return tdTable;
+
+        }
+
+        /// <summary>
+        /// データの保存先とファイル名を指定する
+        /// </summary>
+        /// <returns>csvPath:データパス</returns>
+        public static string CsvFileSave()
+        {
+            string csvPath = "";
+            // 保存先とファイル名を指定する
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.FileName = "TodoList.csv";
+            sfd.Filter = "csvファイル(*.csv)|*.*";
+            sfd.Title = "保存先を指定してください";
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                csvPath = sfd.FileName;
+            }
+            return csvPath;
+        }
+    }
+}
---
+++ WindowsFormsApp1_h/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnRedisplay = new System.Windows.Forms.Button();
+            this.btnReload = new System.Windows.Forms.Button();
+            this.statusBar = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.statusBar.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnSave
+            // 
+            this.btnSave.Font = new System.Drawing.Font("MS UI Gothic", 14F);
+            this.btnSave.Location = new System.Drawing.Point(555, 325);
+            this.btnSave.Margin = new System.Windows.Forms.Padding(5);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(362, 86);
+            this.btnSave.TabIndex = 0;
+            this.btnSave.Text = "保存";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.BtnSave_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("MS UI Gothic", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(47, 53);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(5);
+            this.dataGridView1.Name = "dataGridView1";
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("MS UI Gothic", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.RowHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.dataGridView1.RowTemplate.Height = 21;
+            this.dataGridView1.Size = new System.Drawing.Size(467, 478);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.Statusbar_Update);
+            this.dataGridView1.RowsAdded += new System.Windows.Forms.DataGridViewRowsAddedEventHandler(this.Statusbar_Update);
+            this.dataGridView1.RowsRemoved += new System.Windows.Forms.DataGridViewRowsRemovedEventHandler(this.Statusbar_Update);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("MS UI Gothic", 14F);
+            this.btnAdd.Location = new System.Drawing.Point(555, 208);
+            this.btnAdd.Margin = new System.Windows.Forms.Padding(5);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(168, 89);
+            this.btnAdd.TabIndex = 3;
+            this.btnAdd.Text = "登録";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.BtnAdd_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Font = new System.Drawing.Font("MS UI Gothic", 14F);
+            this.btnDelete.Location = new System.Drawing.Point(749, 208);
+            this.btnDelete.Margin = new System.Windows.Forms.Padding(5);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(168, 89);
+            this.btnDelete.TabIndex = 4;
+            this.btnDelete.Text = "削除";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.BtnDelete_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Font = new System.Drawing.Font("MS UI Gothic", 14F);
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker1.Location = new System.Drawing.Point(555, 78);
+            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(5);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(362, 26);
+            this.dateTimePicker1.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(555, 55);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 16);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "期限";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("MS UI Gothic", 14F);
+            this.textBox1.Location = new System.Drawing.Point(555, 148);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(5);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(362, 26);
+            this.textBox1.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS UI Gothic", 14F);
+            this.label2.Location = new System.Drawing.Point(555, 124);
+            this.label2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 19);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "やること";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("MS UI Gothic", 14F);
+            this.label3.Location = new System.Drawing.Point(43, 24);
+            this.label3.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(103, 19);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "やることリスト";
+            // 
+            // btnRedisplay
+            // 
+            this.btnRedisplay.Location = new System.Drawing.Point(158, 19);
+            this.btnRedisplay.Name = "btnRedisplay";
+            this.btnRedisplay.Size = new System.Drawing.Size(161, 28);
+            this.btnRedisplay.TabIndex = 10;
+            this.btnRedisplay.Text = "完了タスク再表示";
+            this.btnRedisplay.UseVisualStyleBackColor = true;
+            this.btnRedisplay.Click += new System.EventHandler(this.BtnRedisplay_Click);
+            // 
+            // btnReload
+            // 
+            this.btnReload.Location = new System.Drawing.Point(341, 19);
+            this.btnReload.Name = "btnReload";
+            this.btnReload.Size = new System.Drawing.Size(119, 28);
+            this.btnReload.TabIndex = 10;
+            this.btnReload.Text = "再読込";
+            this.btnReload.UseVisualStyleBackColor = true;
+            this.btnReload.Click += new System.EventHandler(this.BtnReload_Click);
+            // 
+            // statusBar
+            // 
+            this.statusBar.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusBar.Location = new System.Drawing.Point(0, 568);
+            this.statusBar.Name = "statusBar";
+            this.statusBar.Size = new System.Drawing.Size(1003, 22);
+            this.statusBar.TabIndex = 11;
+            this.statusBar.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(118, 17);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1003, 590);
+            this.Controls.Add(this.statusBar);
+            this.Controls.Add(this.btnReload);
+            this.Controls.Add(this.btnRedisplay);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnSave);
+            this.Font = new System.Drawing.Font("MS UI Gothic", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "Form1";
+            this.Text = "ToDoリスト";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.statusBar.ResumeLayout(false);
+            this.statusBar.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnRedisplay;
+        private System.Windows.Forms.Button btnReload;
+        private System.Windows.Forms.StatusStrip statusBar;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+    }
+}
+
